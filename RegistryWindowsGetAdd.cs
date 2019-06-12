a07b1dc2e2a825f38043d2a3c65df4c486e4e8a9
drpp12 <deyvi_royer@hotmail.com>
2019-06-11 20:52:01 -0400

    Create RegistryWindowsGetAdd.cs

---
+++ RegistryWindowsGetAdd.cs
@@ -0,0 +1,32 @@
+using Microsoft.Win32;
+
+public class RegistryWindowsGetAdd
+{
+    ///Declaracion de Keys
+    const string userRoot = "HKEY_CURRENT_USER";
+    const string subkey = "KeyNameXX";
+        const string keyName = userRoot + "\\" + subkey;
+
+    //Get Value
+    private string GetKeys()
+    {
+        string Key = "";
+        string KeyNameXX = (string)Registry.GetValue(keyName, "KeyNameXX", "null");
+        if (KeyNameXX != null)
+        {
+            Key = KeyNameXX;
+        }
+        return Key;
+    }
+
+    //Add or Edit Value
+    private void SetRegister(string TD)
+    {
+        string KeyNameXX = (string)Registry.GetValue(keyName, "KeyNameXX", "null");
+        if (KeyNameXX == null)
+        {
+            Registry.SetValue(keyName, "KeyNameXX", TD);
+        }
+    }
+
+}
